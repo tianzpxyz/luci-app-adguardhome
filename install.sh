0a4d04716ee4b2777435df8d26c0fc3e58284656
PPangel <zeniths-puffer-1x@icloud.com>
2026-02-09 10:39:15 +0900

    安装脚本

--- install.sh
+++ install.sh
@@ -4,14 +4,20 @@ set -e
 REPO="w9315273/luci-app-adguardhome"
 API="https://api.github.com/repos/${REPO}/releases/latest"
 
+echo "Getting latest release..."
 URL="$(
   uclient-fetch -qO- "$API" \
   | jsonfilter -e '@.assets[@.name~="^luci-app-adguardhome-.*\\.apk$"].browser_download_url' \
   | head -n1
 )"
 
-[ -n "$URL" ] || { echo "ERR: no .apk found in latest release assets"; exit 1; }
+[ -n "$URL" ] || { echo "ERROR: No .apk found in latest release"; exit 1; }
 
+echo "Downloading: ${URL##*/}"
 uclient-fetch -O /tmp/agh.apk "$URL"
+
+echo "Installing..."
 apk add --allow-untrusted /tmp/agh.apk
 rm -f /tmp/agh.apk
+
+echo "Done!"
