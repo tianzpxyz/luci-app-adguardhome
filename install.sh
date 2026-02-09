60085c824df4d59a462722c9afae18f04fae0507
PPangel <zeniths-puffer-1x@icloud.com>
2026-02-09 10:29:47 +0900

    安装脚本

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 REPO="w9315273/luci-app-adguardhome"
-URL=$(uclient-fetch -qO- "https://api.github.com/repos/${REPO}/releases/latest" | sed -n 's/.*"browser_download_url": *"\([^"]*\.apk\)".*/\1/p' | head -n1)
+URL=$(uclient-fetch -qO- "https://api.github.com/repos/${REPO}/releases/latest" | awk -F'"' '/browser_download_url.*\.apk/ {print $4; exit}')
 uclient-fetch -O /tmp/agh.apk "$URL" && apk add --allow-untrusted /tmp/agh.apk && rm -f /tmp/agh.apk
